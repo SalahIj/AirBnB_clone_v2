ad0f2ead966e27f77e4fc03e7b7437b0045bd41b
Ijbar Salah Eddine <ijbarsalaheddine@gmail.com>
2024-02-18 10:47:21 +0000

    Task_7

--- models/city.py
+++ models/city.py
@@ -1,9 +1,32 @@
-#!/usr/bin/python3
-""" City Module for HBNB project """
-from models.base_model import BaseModel
+#!/usr/bin/python
+""" The imported modules """
+import models
+import sqlalchemy
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
+from models.base_model import BaseModel, Base
+from os import getenv
 
 
-class City(BaseModel):
-    """ The city class, contains state ID and name """
-    state_id = ""
-    name = ""
+class City(BaseModel, Base):
+    """ Class representation
+    Args:
+        BaseModel:
+        Base:
+    """
+    if models.storage_t == "db":
+        __tablename__ = 'cities'
+        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+        name = Column(String(128), nullable=False)
+        places = relationship("Place", backref="cities")
+    else:
+        state_id = ""
+        name = ""
+
+    def __init__(self, *args, **kwargs):
+        """ The __init__ method
+        Args:
+            args:
+            kwargs:
+        """
+        super().__init__(*args, **kwargs)
